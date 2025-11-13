bb2272a3cdeb4d22431c2d3b42d9ac66753e464f
joravv <joravulaa@gmail.com>
2025-11-13 20:36:43 +0100

    mod35

---
+++ mod35/main.js
@@ -0,0 +1,26 @@
+function openMenu(){
+    var menu=document.getElementById("myLinks")
+
+    if(menu.style.display=== "block"){
+        menu.style.display= "none";
+}else{
+menu.style.display= "block"
+}
+}
+
+function search(){
+    var input,filter,ul,li,a,i,txtValue;
+    input =document.getElementById("myInput");
+    filter=input.value.toUperCase();
+    ul=document.getElementById("myList");
+    li=ul.getElementByTagName("li")
+    for(i=0; i<li.length;i++){
+        a=li[i];
+        txtValue=a.textContent;
+        if(text.toUpperCase().indexOf(filter)> -1){
+            li[i].style.display="block";
+        }else{
+            li[i].style.display="none";
+        }
+    }
+}
