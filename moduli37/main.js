a6e14ff42dbd30c182857a8842dbcfee0858e508
joravv <joravulaa@gmail.com>
2025-11-25 20:19:24 +0100

    moduli38

---
+++ moduli37/main.js
@@ -0,0 +1,40 @@
+$(window).ready(function(){
+    alert('It is working!!!');
+});
+
+
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordian').accordian({
+        collapsible: true
+
+    });
+})
+
+
+$(function(){
+    $("#tabs").tabs();
+}
+);
+
+$(function(){
+    $("#tabs").accordion({
+        collapsible: true
+    });
+}
+);
